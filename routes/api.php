45d9968527d1eda209bfd3e734a1dbff4e426fb3
AmriShanab <amrishanab@gmail.com>
2026-01-20 13:50:43 +0530

    Responsive Fixes

--- routes/api.php
+++ routes/api.php
@@ -82,7 +82,7 @@ Route::prefix('v1')->group(function () {
             Route::post('/{id}/accept', 'accept');
             Route::get('/{id}/join', 'getJoinDetails');
             Route::get('/my-history', [AppointmentController::class, 'getAppointmentsOfUser']);
-            Route::post('/appointment/close', [App\Http\Controllers\AppointmentController::class, 'closeAppointment']);
+            Route::post('/appointment/close', [App\Http\Controllers\AppointmentController::class, 'closingAppointments']);
         });
     });
 
