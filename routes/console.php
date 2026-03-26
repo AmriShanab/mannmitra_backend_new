fc2f50db34ecce0ae02829860c21f065dcd9f669
AmriShanab <amrishanab@gmail.com>
2026-03-26 11:55:08 +0530

    The time changed for notification

--- routes/console.php
+++ routes/console.php
@@ -8,7 +8,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:send-wellness-reminders')->dailyAt('11:08')->timezone('Asia/Kolkata');
+Schedule::command('app:send-wellness-reminders')->dailyAt('17:00')->timezone('Asia/Kolkata');
 // Schedule::command('app:generate-weekly-reflections')->weeklyOn(0, '18:00')->timezone('Asia/Kolkata');
 Schedule::command('companion:generate-weekly-journals')->weeklyOn(0, '23:59')->timezone('Asia/Kolkata');
 Schedule::command('closed:expired:appointments')->daily();
