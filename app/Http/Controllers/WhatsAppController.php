a65d157b69324242bc1923f9d15cb691646b3f89
AmriShanab <amrishanab@gmail.com>
2026-05-20 13:57:48 +0530

    WHatsapp Chatbot feature

--- app/Http/Controllers/WhatsAppController.php
+++ app/Http/Controllers/WhatsAppController.php
@@ -39,7 +39,8 @@ class WhatsAppController extends Controller
         $cleanNumber = explode('@', $senderId)[0];
 
         $allowedNumbers = [
-            '94722561060', // Musharraf's number
+            '94722561060',
+            '94783099340'
         ];
 
         if (!in_array($cleanNumber, $allowedNumbers)) {
